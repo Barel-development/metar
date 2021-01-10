9efa7bd76d2928ea611bb8c110eaf5b9bd51d6af
Bara Maresova <bbmaresova@gmail.com>
2021-01-10 10:50:35 +0100

    draft of folders and components structure

---
+++ src/components/Airport/Airport.jsx
@@ -0,0 +1,11 @@
+import { React } from 'react';
+import { Metar } from '../Metar/Metar.jsx';
+
+export const Airport = () => {
+  return (
+    <>
+      <div>Airport</div>
+      <Metar />
+    </>
+  );
+};
